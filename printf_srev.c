3a3825b4c8dcf4c06bcbd93873859bbc4125d6cb
Abel-G <56924148+Abel-G@users.noreply.github.com>
2022-08-17 22:19:56 +0300

    serv

--- printf_srev.c
+++ printf_srev.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * printf_srev - function that prints a str in reverse
- * @args: type struct va_arg where is allocated printf arguments
- *
- * Return: the string
- */
+* printf_srev - function that prints a str in reverse
+* @args: type struct va_arg where is allocated printf arguments
+* Return: the string
+*/
 int printf_srev(va_list args)
 {
 char *s = va_arg(args, char*);
