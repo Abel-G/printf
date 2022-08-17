28d07ed0d185c03dabe10dea7fa59fab9c6bc713
Abel-G <56924148+Abel-G@users.noreply.github.com>
2022-08-17 22:22:15 +0300

    string

--- printf_exclusive_string.c
+++ printf_exclusive_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * printf_exclusive_string - print exclusuives string.
- * @val: argumen t.
- * Return: the length of the string.
- */
+* printf_exclusive_string - print exclusuives string.
+* @val: argumen t.
+* Return: the length of the string.
+*/
 int printf_exclusive_string(va_list val)
 {
 char *s;
