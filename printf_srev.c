b9d2ad38b79f7629c13766938b7fca5617c28f48
Abel-G <56924148+Abel-G@users.noreply.github.com>
2022-08-17 22:16:36 +0300

    print serv

--- printf_srev.c
+++ printf_srev.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * printf_srev - function that prints a str in reverse
@@ -8,16 +8,14 @@
  */
 int printf_srev(va_list args)
 {
-
-	char *s = va_arg(args, char*);
-	int i;
-	int j = 0;
-
-	if (s == NULL)
-		s = "(null)";
-	while (s[j] != '\0')
-		j++;
-	for (i = j - 1; i >= 0; i--)
-		_putchar(s[i]);
-	return (j);
+char *s = va_arg(args, char*);
+int i;
+int j = 0;
+if (s == NULL)
+s = "(null)";
+while (s[j] != '\0')
+j++;
+for (i = j - 1; i >= 0; i--)
+_putchar(s[i]);
+return (j);
 }
