4f0b4ae5745483868fa9390e40703f16561936ae
Abel <abeljeb@gmail.com>
2022-08-17 10:39:33 -0700

    print

--- _printf.c
+++ _printf.c
@@ -4,7 +4,7 @@
 * @format: identifier
 * Return: length of string
 */
-int _printf(const char *const format, ...)
+int _printf(const char * const format, ...)
 {
 convert_match m[] = {
 {"%s", printf_string}, {"%c", printf_char},
