8166ea56d8d9a0a6a7b9d51848901db13863e333
Abel <abeljeb@gmail.com>
2022-08-17 18:45:00 -0700

    print

--- printf_pointer.c
+++ printf_pointer.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
  * printf_pointer - prints an hexgecimal number.
  * @val: arguments.
@@ -7,25 +6,23 @@
  */
 int printf_pointer(va_list val)
 {
-	void *p;
-	char *s = "(nil)";
-	long int a;
-	int b;
-	int i;
-
-	p = va_arg(val, void*);
-	if (p == NULL)
-	{
-		for (i = 0; s[i] != '\0'; i++)
-		{
-			_putchar(s[i]);
-		}
-		return (i);
-	}
-
-	a = (unsigned long int)p;
-	_putchar('0');
-	_putchar('x');
-	b = printf_hex_aux(a);
-	return (b + 2);
+void *p;
+char *s = "(nil)";
+long int a;
+int b;
+int i;
+p = va_arg(val, void*);
+if (p == NULL)
+{
+for (i = 0; s[i] != '\0'; i++)
+{
+_putchar(s[i]);
+}
+return (i);
+}
+a = (unsigned long int)p;
+_putchar('0');
+_putchar('x');
+b = printf_hex_aux(a);
+return (b + 2);
 }
